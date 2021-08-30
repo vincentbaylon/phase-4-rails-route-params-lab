39c48e430cba5eb8266e6be8f2ec88a0090db7d0
vincentbaylon <vincentbaylon@gmail.com>
2021-08-30 05:13:28 -0700

    commit tree a9fce752e5f058571322258f52e7f77010bac5f7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      find_student = Student.where(first_name: params[:name]).or(Student.where(last_name: params[:name]))
+      render json: find_student
+    else
+      students = Student.all
+      render json: students
+    end
+
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
